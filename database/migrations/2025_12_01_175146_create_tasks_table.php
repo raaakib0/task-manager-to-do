b113d616d501a48c30d3efa14302c551c75786cd
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-02 00:16:49 +0600

    added completed column in task table

--- database/migrations/2025_12_01_175146_create_tasks_table.php
+++ database/migrations/2025_12_01_175146_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }

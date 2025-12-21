43cf1193d7888b1aa2e636d1847e2bf66cbac9d9
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-21 23:24:13 +0600

    task form formated

--- resources/js/TaskForm.vue
+++ resources/js/TaskForm.vue
@@ -1,23 +1,23 @@
 <template>
     <form submit.prevent="submit">
-<input v-model="title" placeholder="Task Title"required />
-<button type="submit" >Add</button>
+        <input v-model="title" placeholder="Task Title" required />
+        <button type="submit">Add</button>
     </form>
 </template>
 <script lang="ts">
-    import{defineComponent, ref} from 'vue';
-    import axios from 'axios';
-    export default defineComponent({
+import axios from 'axios';
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
     emits: ['created'],
-    setup(_,{emit}){
-    const title=ref('');
-    const submit = async()=>{
-    if(!title.value) return;
-    await axios.post('/api/tasks', {title: title.value});
-    title.value ='';
-    emit('created');
-};
-return {title,submit};
-}
-    });
+    setup(_, { emit }) {
+        const title = ref('');
+        const submit = async () => {
+            if (!title.value) return;
+            await axios.post('/api/tasks', { title: title.value });
+            title.value = '';
+            emit('created');
+        };
+        return { title, submit };
+    },
+});
 </script>

b90442e15909196a24aff9be58094ca068185819
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-22 22:55:09 +0600

    heading change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,7 @@
 <body>
     <div class="container">
         <div class="box">
-            <h1>Welcome!</h1>
+            <h1>Welcome!TO Task Manager</h1>
             <p>You have successfully created a Laravel Blade page.</p>
         </div>
     </div>

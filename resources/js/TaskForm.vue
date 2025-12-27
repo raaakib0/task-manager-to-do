5129835dd9979637441b0bdd7025fd673d854321
Md. Rakibul Islam <raaakib0@gmail.com>
2025-12-27 21:33:26 +0600

    clean code

--- resources/js/TaskForm.vue
+++ resources/js/TaskForm.vue
@@ -9,16 +9,6 @@ import axios from 'axios';
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
     emits: ['created'],
-    // setup(_, { emit }) {
-    //     const title = ref('');
-    //     const submit = async () => {
-    //         if (!title.value) return;
-    //         await axios.post('/api/tasks', { title: title.value });
-    //         title.value = '';
-    //         emit('created');
-    //     };
-    //     return { title, submit };
-    // },
     setup(_,{emit}){
         const title = ref('');
         const submit = async() =>{
